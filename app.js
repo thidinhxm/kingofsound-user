e24e8ce57b4a2a63e3df159a8869b896c65ab2e7
thidinhxm <thidinhxm@gmail.com>
2021-11-16 14:05:33 +0700

    Revise router

--- app.js
+++ app.js
@@ -27,21 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/products', productsRouter)
 
-app.get('/checkout', (req, res, next) => {
-	res.render('checkout')
-})
-
-app.get('/blank', function(req, res, next) {
-	res.render('blank')
-})
-
-app.get('/login', (req, res, next) => {
-	res.render('login')
-})
-
-app.get('/register', (req, res, next) => {
-	res.render('register')
-})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
 	next(createError(404));
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/checkout', (req, res, next) => {
+	res.render('checkout')
+})
+
+router.get('/blank', function(req, res, next) {
+	res.render('blank')
+})
+
+router.get('/login', (req, res, next) => {
+	res.render('login')
+})
+
+router.get('/register', (req, res, next) => {
+	res.render('register')
+})
 module.exports = router;
 
