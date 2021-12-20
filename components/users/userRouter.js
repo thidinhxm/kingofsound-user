0404882fc74073efd6307bda3501c8b6eaf9430e
thidinhxm <thidinhxm@gmail.com>
2021-12-20 17:17:43 +0700

    change view

--- components/users/userRouter.js
+++ components/users/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+
 const userController = require('./userControler');
 
 router.get('/', userController.isLogin, userController.profile);
