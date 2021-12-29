5f1789b984d1ef32c7984f3c23ab924e8d9c3556
thidinhxm <thidinhxm@gmail.com>
2021-12-29 23:56:02 +0700

    fix quantity input number

--- public/js/main.js
+++ public/js/main.js
@@ -102,8 +102,30 @@
 
 	/////////////////////////////////////////
 
+
+	$('.input-number.qty-cart').each(function() {
+		var $this = $(this),
+		$input = $this.find('input[type="number"]'),
+		up = $this.find('.qty-up'),
+		down = $this.find('.qty-down');
+
+		down.on('click', function () {
+			var value = parseInt($input.val()) - 1;
+			value = value < 1 ? 1 : value;
+			$input.val(value);
+			$input.change();
+			updatePriceSlider($this , value)
+		})
+
+		up.on('click', function () {
+			var value = parseInt($input.val()) + 1;
+			$input.val(value);
+			$input.change();
+			updatePriceSlider($this , value)
+		})
+	});
 	// Input number
-	$('.input-number').each(function() {
+	$('.input-number.prices').each(function() {
 		var $this = $(this),
 		$input = $this.find('input[type="number"]'),
 		up = $this.find('.qty-up'),
