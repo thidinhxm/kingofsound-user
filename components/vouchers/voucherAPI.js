7a79d2c3bc28a97fc5fa6a54336093b849db29ec
Hoang Truong <hoangtruongvann@gmail.com>
2022-01-02 09:24:54 +0700

    check date valid of voucher

--- components/vouchers/voucherAPI.js
+++ components/vouchers/voucherAPI.js
@@ -1,22 +1,23 @@
 
 const voucherService = require('./voucherService');
 
-exports.checkValidVoucher =  async(req,res,next) =>
-{
+exports.checkValidVoucher = async (req, res, next) => {
     try {
-       const {voucher_id} = req.body;
-       const voucher = await voucherService.getVoucher(voucher_id);
-       if(voucher)
-        res.json(
-            {
-                success: true,
-                discount:voucher.discount
-            }
-        )
+        const today = new Date();
+        const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        const { voucher_id } = req.body;
+        const voucher = await voucherService.checkValidVoucher(voucher_id,date);
+        if (voucher)
+            res.json(
+                {
+                    success: true,
+                    discount: voucher.discount
+                }
+            )
         else
             res.json({
                 success: false,
-                discount :0
+                discount: 0
             })
     }
     catch (err) {
