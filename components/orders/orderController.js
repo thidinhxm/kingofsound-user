da18f9785fe6d02e639da72b02f0a86a3b29934c
Hoang Truong <hoangtruongvann@gmail.com>
2022-01-12 19:55:40 +0700

    fix validate add review after order complete

--- components/orders/orderController.js
+++ components/orders/orderController.js
@@ -6,8 +6,12 @@ exports.list = async (req, res, next) => {
         const message = req.query.message;
         const user = req.user;
         const user_id = user.user_id;
-        const listOrder =  await orderSevice.listOrder(user_id); 
-        res.render('../components/orders/orderViews/listOrder',{ listOrder,message });
+        const listOrder = await orderSevice.listOrder(user_id);
+        listOrder.forEach(element => {
+            if(element.order_status == "Đã giao")
+            element.is_complete = 1;
+        });
+        res.render('../components/orders/orderViews/listOrder', { listOrder, message });
     }
     catch (error) {
         next(error);
@@ -16,31 +20,34 @@ exports.list = async (req, res, next) => {
 exports.detail = async (req, res, next) => {
     try {
         const order_id = req.params.order_id;
-        const detailOrder = await orderSevice.detailOrder(order_id);
-        const order =  await orderSevice.order(order_id);
+        let detailOrder = await orderSevice.detailOrder(order_id);
+        let order = await orderSevice.order(order_id);
         const voucher = await voucherService.getVoucher(order.voucher);
-        res.render('../components/orders/orderViews/detailOrder',{ detailOrder, order, voucher});
+        if (order.order_status == "Đã giao") {
+            detailOrder.forEach(element => {
+                element.is_complete = 1;
+            });
+        }
+        res.render('../components/orders/orderViews/detailOrder', { detailOrder, order, voucher});
     }
     catch (error) {
         next(error);
     }
 }
 
-exports.review = async (req,res,next) =>
-{
-    try
-    {
-    const product_id = req.body.product_id;
-    const order_id = req.body.order_id;
-    const content = req.body.review;
-    const rating = req.body.star;
-    console.log({product_id:product_id,content:content,rating:rating,user_id:req.user.user_id});
-    const review = await reviewService.addReview({product_id:product_id,content:content,rating:rating,user_id:req.user.user_id});
-    const review_id = review.review_id;
-    await orderSevice.reviewDetailOrder(order_id,product_id, review_id);
-    res.redirect('/orders/' + order_id);
+exports.review = async (req, res, next) => {
+    try {
+        const product_id = req.body.product_id;
+        const order_id = req.body.order_id;
+        const content = req.body.review;
+        const rating = req.body.star;
+        console.log({ product_id: product_id, content: content, rating: rating, user_id: req.user.user_id });
+        const review = await reviewService.addReview({ product_id: product_id, content: content, rating: rating, user_id: req.user.user_id });
+        const review_id = review.review_id;
+        await orderSevice.reviewDetailOrder(order_id, product_id, review_id);
+        res.redirect('/orders/' + order_id);
     }
     catch (error) {
-    next(error);
+        next(error);
     }
 }
